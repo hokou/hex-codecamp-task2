0c9061b1f10dfa34971bddff3387b679e0599596
hokou <hokou.shiau@gmail.com>
2022-06-15 11:20:23 +0800

    fix cost calcu error

--- js/all.js
+++ js/all.js
@@ -1,6 +1,6 @@
 
-const cost_base = [0.6, 0.55, 0.5, 0.4]
-const cost_normal = [1.6, 1.55, 1.5, 1.4]
+const cost_base = [0.06, 0.05, 0.04, 0.035]
+const cost_normal = [0.16, 0.15, 0.14, 0.135]
 
 $(document).ready(function () {
   $('.btn-top').click(function (e) {
@@ -32,6 +32,8 @@ $(document).ready(function () {
 
 function cost_calu(text, num){
   $('.user-number span').text(text+' ');
-  $('.cost-number1 span').text(parseInt(text)*cost_base[num]);
-  $('.cost-number2 span').text(parseInt(text)*cost_normal[num]);
+  $('.cost-number1 span').text(parseInt(parseInt(text)*cost_base[num])+' ');
+  $('.cost-number2 span').text(parseInt(parseInt(text)*cost_normal[num])+' ');
+  $('.cost-number1 sub').text(' TWD');
+  $('.cost-number2 sub').text(' TWD');
 }
